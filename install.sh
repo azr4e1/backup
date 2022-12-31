61d51d6629e9796e9c7a64c8c12a0fba5bfded9d
Personal <drumondlorenzo@posteo.net>
2022-12-31 22:16:31 +0000

    Added encryption

--- install.sh
+++ install.sh
@@ -5,17 +5,19 @@
 
 # check if .config/backup exists,
 # otherwise create it
-[ -d "$HOME/.config/backup" ] || { mkdir -p $HOME/.config/backup; touch $HOME/.config/backup/conf; }
+[ -d "$HOME/.config/backup" ] || { mkdir -p $HOME/.config/backup; touch $HOME/.config/backup/{conf,encryption}; }
 
-optstring=":hu"
+[ -d "$HOME/.local/bin" ] || { mkdir -p $HOME/.local/bin; }
+
+optstring=":hue:"
 while getopts $optstring name; do
     case $name in
-        h) echo "Installer for backup cli utility: -h for help; -u to install as user"; exit;;
-        u) /usr/bin/cp ./backup $HOME/.local/bin/backup && echo "Installation complete"; exit;;
-        *) echo "$OPTARG is not a valid option"; usage; exit 1;;
+        h) echo "Installer for backup cli utility: -h for help; -e to add encryption id"; exit;;
+        e) echo $OPTARG > $HOME/.config/backup/encryption;;
+        ?) echo "$OPTARG is not a valid option"; usage; exit 1;;
+        :) echo "$OPTARG is not a valid option"; usage; exit 1;;
     esac
 done
 
-# install it
-sudo /usr/bin/cp ./backup /usr/bin/backup
-echo "Installation complete"
+/usr/bin/cp ./backup $HOME/.local/bin/backup
+echo "Installation complete";
